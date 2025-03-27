f0da0fb2677acf6ad973a9562bf8c363ae83d217
Alejo Castelnovo <castelnovo12@gmail.com>
2025-03-27 19:10:45 -0300

    Cambios Gallery, Carrousel

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -1,7 +1,36 @@
-import { Image } from "./image";
 import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { Image } from "./image";
 
 export const Gallery = (props) => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
     <div id="portfolio" className="text-center">
       <div className="container">
@@ -10,26 +39,18 @@ export const Gallery = (props) => {
           <p>Explora nuestras fotos y videos destacados.</p>
         </div>
         <div className="row">
-
-
           <div className="portfolio-items">
-            {props.data
-              ? props.data.map((d, i) => (
-                <div
-                  key={`${d.title}-${i}`}
-                  className="col-sm-6 col-md-4 col-lg-4"
-                >
-                  <Image
-                    title={d.title}
-                    smallImage={d.smallImage}
-                  />
-                </div>
-              ))
-              : "Loading..."}
-          </div>
-          <div className="col-md-4">
-            <div className="portfolio-item">
-            </div>
+            {props.data ? (
+              <Slider {...settings}>
+                {props.data.map((d, i) => (
+                  <div key={`${d.title}-${i}`}>
+                    <Image title={d.title} smallImage={d.smallImage} />
+                  </div>
+                ))}
+              </Slider>
+            ) : (
+              "Loading..."
+            )}
           </div>
         </div>
       </div>
